f30e7cdee9516fe1a5fe5d01eea2908dce764361
Mohammad Mostafa Dastjerdi <ezmmd83@gmail.com>
2021-08-31 22:48:17 +0430

    v1.0.0-beta1 completed basic functionality as expected

--- LightTest/UnitTest.php
+++ LightTest/UnitTest.php
@@ -3,45 +3,88 @@
 namespace LightTest;
 
 use ReflectionClass;
+use ReflectionMethod;
+use LightTest\Exception\FailedTestException;
 
 abstract class UnitTest
 {
-	private int $testNumber = 0;
+	use AssertionUtil;
 	
-	private function printResult($color, $message)
-	{
-		print "<div>$message</div>";
-	}
-	
-	public function runTests(): void
+	public function run(): void
 	{
 		$reflect = new ReflectionClass($this);
-		$testMethods = array_filter($reflect->getMethods(), function ($value) {
-			return str_starts_with($value->name, "test_");
-		});
 		
-		foreach ($testMethods as $testMethod) {
-			$this->testNumber++;
-			$reason = "";
-			$methodName = $testMethod->name;
+		$this->sectionStart($reflect->name);
+		
+		$testMethods = array_filter($reflect->getMethods(), fn($value) => str_starts_with($value->name, "test_"));
+		$testsCount = count($testMethods);
+		$padLength = (int)log10($testsCount) + 1;
+		
+		for ($i = 0; $i < $testsCount; $i++) {
+			$methodName = $testMethods[$i]->name;
 			$testName = substr($methodName, 5);
+			$no = str_pad((string)($i + 1), $padLength, "0", STR_PAD_LEFT);
+			
+			$info = str_replace("\n", "<br>", (new ReflectionMethod($this, "$methodName"))->getDocComment());
+			/*if ($info) { # todo: needs more care
+				$tmp = explode("\n", $info);
+				for ($n = 0; $n < count($tmp); $n++) $tmp[$n] = trim($tmp[$n], "/* ");
+				$info = trim(implode("\n", $tmp));
+			} else $info = "No Info.";*/
+			if (!$info) $info = "No Info.";
 			
 			try {
-				$this->$methodName($reason);
-				$this->printResult("green", "#$this->testNumber | Test($testName) Passed!");
-			} catch (TestException $e) {
-				$this->printResult("red", "#$this->testNumber | Test($testName) Failed: $e->reason");
+				$this->$methodName();
+				$this->printResult(true, $no, $testName, $info);
+			} catch (FailedTestException $e) {
+				$this->printResult(false, $no, $testName, $info, $e->getMessage());
 			}
 		}
+		
+		$this->sectionEnd();
 	}
 	
-	protected function expect($input, $expectation): void
+	private function sectionStart(string $name)
 	{
-		if ($input != $expectation) throw new TestException("$input != $expectation");
+		print <<<HTML
+			<section>
+				<div class="title">
+					<span class="collapsed"></span>
+					<div>$name</div>
+					<hr />
+				</div>
+				
+				<div class="content" style="height: 0;">
+					<table>
+		HTML;
 	}
 	
-	protected function expectExact($input, $expectation): void
+	private function sectionEnd()
 	{
-		if ($input !== $expectation) throw new TestException("$input !== $expectation");
+		print <<<HTML
+					</table>
+				</div>
+			</section>
+		HTML;
+	}
+	
+	private function printResult(bool $success, string $no, string $name, string $info, string $errInfo = null)
+	{
+		if ($success) {
+			$icon = "&check;";
+			$status = "s";
+		} else {
+			$icon = "&times;";
+			$status = "f";
+		}
+		
+		print <<<HTML
+			<tr>
+				<td class="icon" data-icon="$status" data-info="$errInfo">$icon</td>
+				<td class="number">$no</td>
+				<td class="name">$name</td>
+				<td class="info" data-info="$info">?</td>
+			</tr>
+		HTML;
 	}
 }
